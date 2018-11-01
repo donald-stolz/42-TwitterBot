d27f2d3f794b5dd1bb410ccfb60c485af9dc293f
Donald Stolz <donaldtstolz@gmail.com>
2018-11-01 10:35:13 -0700

    Built basic favoriting bot

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
+// config.js
+
 module.exports = {
     consumer_key: '2BjRTIDtRM0cL0iGWQklXbY2s',
-    consumer_secret: '',
+    consumer_secret: process.env.CONSUMER_SECRET,
     access_token_key: '328817860-dmSaCL2xIkgUHeawzA8O4Q7Rz5PqiJt67htaAbq9',
-    access_token_secret: '',
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 };
