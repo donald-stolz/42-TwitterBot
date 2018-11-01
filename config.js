2075818eb2a65c31e09b9587969b596810c0724d
Donald Stolz <donaldtstolz@gmail.com>
2018-11-01 09:19:48 -0700

    Intial setup for repo

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: '2BjRTIDtRM0cL0iGWQklXbY2s',
+    consumer_secret: '',
+    access_token_key: '328817860-dmSaCL2xIkgUHeawzA8O4Q7Rz5PqiJt67htaAbq9',
+    access_token_secret: '',
+};
