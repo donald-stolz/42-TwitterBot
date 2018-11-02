c6a8e9c562ee565cf829ede3dcc81896ee40dfd0
Donald Stolz <donaldtstolz@gmail.com>
2018-11-01 19:04:39 -0700

    Found new resources to build more robust bot

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # 42-TwitterBot
+
 Node.js Twitter bot created for 42 - Silicon Valley's Yellow Brick Road project
+
+## Packages
+
+[twit](https://www.npmjs.com/package/twit)  
+[dotenv](https://www.npmjs.com/package/dotenv)
+
+## Learning Resources
+
+[twitter-bot-playground](https://spences10.gitbooks.io/twitter-bot-playground/content/)  
+[awesome-twitter-bots](https://github.com/amandeepmittal/awesome-twitter-bots)  
+[Build a simple Twitter Bot](https://codeburst.io/build-a-simple-twitter-bot-with-node-js-in-just-38-lines-of-code-ed92db9eb078)
--- app.js
+++ app.js
@@ -1,39 +1,6 @@
 // app.js
 require('dotenv').load();
-const Twitter = require('twitter');
+const Twit = require('twit');
 const config = require('./config.js');
 
-const T = new Twitter(config);
-
-// Set up search parameters
-var params = {
-    q: '#nodejs', // Search query (required)
-    count: 10, // Number of tweets to return
-    result_type: 'recent', // returns most recent results
-    lang: 'en', // English
-};
-
-T.get('search/tweets', params, function(err, data, response) {
-    if (!err) {
-        data.statuses.forEach(tweet => {
-            // Get the tweet Id from the returned data
-            let id = { id: tweet.id_str };
-            // Try to favorite selected tweet
-            T.post('favorites/create', id, function(err, response) {
-                if (!err) {
-                    // If the favorite is successful, log the url of the tweet
-                    let username = response.user.screen_name;
-                    let tweetId = response.id_str;
-                    console.log(
-                        'Favorited: ',
-                        `https://twitter.com/${username}/status/${tweetId}`
-                    );
-                } else {
-                    console.error(err[0].message);
-                }
-            });
-        });
-    } else {
-        console.error(err[0].message);
-    }
-});
+const T = new Twit(config);
