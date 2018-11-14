61e8bc3a94d9750df078c6904e87bf552a6b7f1f
Donald Stolz <donaldtstolz@gmail.com>
2018-11-13 17:05:28 -0800

    Updating buzzword check

--- src/tin-man.js
+++ src/tin-man.js
@@ -13,6 +13,7 @@ const hour = 1000 * 60 * 60;
 
 // Tweet every 24 hours?
 
+testFunc();
 // Follow 42 - Favorite every post
 // Retweet any post with more than 3 retweets in 3 hours
 const check42 = async tweet => {
@@ -32,9 +33,11 @@ const check42 = async tweet => {
 // Follow Wired and THN.
 // Retweet/Favorite any tweet that matches a list of tech words
 const checkTech = async tweet => {
+	console.log("Tech Tweet");
+	
     const id = tweet.id_str;
     // Check list
-    if (buzzwords.some(word => tweet.text.includes(word))) {
+	if (buzzwords.some(word => tweet.text.toLowerCase().includes(word))) {
         retweet(id);
         favorite(id);
     } else {
@@ -51,27 +54,46 @@ const checkTech = async tweet => {
     }
 };
 
+
+const checkElon = async tweet => {
+	console.log("Elon Tweet");
+	
+    const id = tweet.id_str;
+    // Check list
+	if (buzzwords.some(word => tweet.text.toLowerCase().includes(word))) {
+        retweet(id);
+        favorite(id);
+    }
+};
+
 // Follow TED Talks
 // Retweet any post with more than N retweets in 2 hours
 const checkTED = tweet => {
+	console.log("Ted Tweet");
+	
     // If more than 30 retweets in an hour, retweet and favorite
-    const id = tweet.id_str;
-    setTimeout(async () => {
-        t = await getStats(id);
-        if (t.retweet_count > 30) {
-            console.log('Favoriting & Retweeting');
-            console.log(t);
-            retweet(id);
-            favorite(id);
-        }
-    }, hour);
+	const id = tweet.id_str;
+	if (buzzwords.some(word => tweet.text.toLowerCase().includes(word))) {
+        retweet(id);
+        favorite(id);
+    } else {
+        setTimeout(async () => {
+            t = await getStats(id);
+            if (t.retweet_count > 30) {
+                console.log('Favoriting & Retweeting');
+                console.log(t);
+                retweet(id);
+                favorite(id);
+            }
+        }, hour);
+    }
 };
 
 stream('42SiliconValley', check42);
 stream('WIRED', checkTech);
 stream('WIREDScience', checkTech);
 stream('TheHackersNews', checkTech);
-stream('elonmusk', checkTech);
+stream('elonmusk', checkElon);
 stream('TEDTalks', checkTED);
 
 // This will allow the bot to run on now.sh
